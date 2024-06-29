26035a53725b0fdd43017537e01f9627585bf57e
MinhPHi <21130471@st.hcmuaf.edu.vn>
2024-06-30 00:26:22 +0700

    Add component header and footer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/header";
+import Footer from "./components/footer";
+import logo from "./logo.svg";
+import React from "react";
+
+function App() {
+return(
+    <div>
+    <Header></Header>
+        <Footer></Footer>
+    </div>
+)
+}
+
+export default App;
