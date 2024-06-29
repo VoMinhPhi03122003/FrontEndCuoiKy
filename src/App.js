83d72f86fc56fe3c43a1664961cf46b5faf83df7
MinhPHi <21130471@st.hcmuaf.edu.vn>
2024-06-30 01:34:26 +0700

    Edit file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import Header from "./components/header";
-import Footer from "./components/footer";
-import logo from "./logo.svg";
-import React from "react";
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-return(
-    <div>
-    <Header></Header>
-        <Footer></Footer>
-    </div>
-)
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
