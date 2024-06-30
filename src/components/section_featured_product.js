d88dec1d121fbb6cf53e0531a081ee3bdf2b20df
MinhPHi <21130471@st.hcmuaf.edu.vn>
2024-06-30 23:36:52 +0700

    nâng cấp component section_featured_product.js

--- src/components/section_featured_product.js
+++ src/components/section_featured_product.js
@@ -1,52 +1,18 @@
 
 import React, {useState} from "react";
+import FeaturedTitle from './featured_title'
+import FeaturedContent from './featured_content'
 function SectionFeaturedCode(){
 
-    // Tạo state địa phương activeItem và hàm setActiveItem để cập nhật giá trị của state
-    const [activeItem, setActiveItem] = useState(null);
 
-    // Hàm xử lý sự kiện click, cập nhật giá trị của activeItem và hiển thị thông báo alert
-    const handleClick = (filter) => {
-        setActiveItem(filter);
-        alert(`Clicked on ${filter}`);
-    };
     return(
         <section className="featured spad">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-12">
-                        <div className="section-title">
-                            <h2> SẢN PHẨM NỔI BẬT</h2>
-                        </div>
-                        <div className="featured__controls">
-                            <ul>
-                                <li
-                                    className={activeItem === ".suckhoethugian" ? "active" : ""}
-                                    data-filter=".suckhoethugian"
-                                    onClick={() => handleClick(".suckhoethugian")}>
-                                    Sức khỏe - thư giản
-                                </li>
-                                <li
-                                    className={activeItem === ".tuichuomnonglanh" ? "active" : ""}
-                                    data-filter=".tuichuomnonglanh"
-                                    onClick={() => handleClick(".tuichuomnonglanh")}>
-                                    Túi chườm nóng - lạnh
-                                </li>
-                                <li
-                                    className={activeItem === ".dogom" ? "active" : ""}
-                                    data-filter=".dogom"
-                                    onClick={() => handleClick(".dogom")}>
-                                    Đồ gốm
-                                </li>
-                                <li
-                                    className={activeItem === ".tranh" ? "active" : ""}
-                                    data-filter=".tranh"
-                                    onClick={() => handleClick(".tranh")}>
-                               Tranh
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                    <FeaturedTitle/>
+                </div>
+                <div className="row featured__filter">
+                    <FeaturedContent/>
                 </div>
             </div>
         </section>
