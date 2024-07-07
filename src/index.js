b1e76c5fe2ff6f12c3cece1bc99a14bc33d65317
MinhPHi <21130471@st.hcmuaf.edu.vn>
2024-07-07 12:18:22 +0700

    Chỉnh sửa phần phân loại sản phẩm

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import './css/style.css'
 const  root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router}/>
     </React.StrictMode>
 );
 
