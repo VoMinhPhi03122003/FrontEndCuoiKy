7fc66160fe4dacbc6db29fc579839d70e47cbd73
MinhPHi <21130471@st.hcmuaf.edu.vn>
2024-07-07 11:25:47 +0700

    lấy danh sách sản phẩm nổi bật từ Json - server

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,14 @@
 import {useDispatch, useSelector} from "react-redux"
 function Cart(){
     const cart = useSelector(state => state.cart);
+
+    /**
+     useSelector là một hook của React Redux,
+     cho phép bạn lấy ra các giá trị từ Redux store.
+     Bằng cách truyền một hàm selector,
+     bạn có thể lựa chọn các phần của state mà bạn muốn truy xuất từ store.
+     */
+
     return (
         <div className="header__cart">
             <ul>
