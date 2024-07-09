7ec7bb94312b3d401e8efb3c19a66856b7d339a0
MinhPHi <21130471@st.hcmuaf.edu.vn>
2024-07-09 22:47:52 +0700

    không cho thêm sản phẩm mới vào giỏ hàng nếu sản phẩm đó đã có trong giỏ hàng , thêm hàm tính tổng giá trị của giỏ hàng

--- src/javascript/utils.js
+++ src/javascript/utils.js
@@ -7,4 +7,22 @@ export function formatCurrency(number) {
 
     return formattedNumber;
 }
+export function checkItemExistCart(cart, product) {
+    /*
+      Sử dụng hàm mảng 'find' để tìm phần tử trong mảng 'cart'
+      có 'id' tương tự với 'id' của 'product.id'
+    */
+    return cart.find(item => item.id === product.id);
+    /*
+       Nếu trong mảng cart không có phần tử nào thỏa mãn điều kiện,
+       thì hàm find sẽ trả về giá trị undefined.
+    */
+}
 
+export function totalPrice(cart){
+    let total = 0;
+    cart.forEach(item => {
+        total += item.price;
+    });
+    return total;
+}
